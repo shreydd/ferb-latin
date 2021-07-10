609d94f61e24647933658ced6bfca01c12b3e20b
shreydd <47713047+shreydd@users.noreply.github.com>
2021-07-10 13:59:39 +0530

    updated style and value

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 var btn = document.querySelector('.btn-translate');
-var inputText = document.querySelector('.inp-text');
+var inputText;
 var outputWindow = document.querySelector('.outputArea');
-// var url = 'https://api.funtranslations.com/translate/ferb-latin.json';
-var url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
+var url = 'https://api.funtranslations.com/translate/ferb-latin.json';
+// var url = 'https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json';
 
 function translateURL(text) {
     return url + '?' + 'text=' + text;
 }
 
+function getinput() {
+    inputText = document.querySelector('.inp-text').value;
+
+}
 
 function translateHandler() {
     // console.log("heere");
     // outputWindow.innerHTML = inputText.value;
-
+    getinput();
     fetch(translateURL(inputText))
         .then(response => response.json())
         .then(jsonData => {
-            outputWindow.innerHTML = jsonData.contents.translated;
-            // console.log(jsonData)
+            // outputWindow.innerHTML = jsonData.contents.translated;
+            console.log(jsonData.contents.translated);
         })
 }
 
